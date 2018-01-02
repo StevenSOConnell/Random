afc565ffe351a943c70af0eafecc5cb380073b61
Steve O'Connell <steveoconnell@steves-imac.local>
2018-01-02 12:04:54 -0500

    Commit some changes

---
+++ Random/README.md
@@ -0,0 +1,14 @@
+#  My Title
+
+Heading
+======
+
+## Sub-heading
+
+This is a paragraph.
+
+Another paragraph.
+
+Second Heading
+============
+More text.
--- Random/main.c
+++ Random/main.c
@@ -10,17 +10,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//#include <Accelerate/Accelerate.h>
+//#include <x86intrin.h>      // all
+//#include <xmmintrin.h>      // SSE
+#include <immintrin.h>      // all
+//#include <avxintrin.h>      // AVX
+//#include <avx2intrin.h>     // AVX-2
+//#include <avx512fintrin.h>  // AVX-512
+
+#ifdef __SSE__
+#pragma message "sse"
+#endif
+#ifdef __AVX__
+#pragma message "avx"
+#endif
+#ifdef __AVX2__
+#pragma message "avx2"
+#endif
+#ifdef __AVX512F__
+#pragma message "avx512f"	// forced compiler flags: -mavx512f (but crashes assert?!), not part of Xcode yet
+#endif
+
+#include <sys/sysctl.h>
+
 static int gRandom;
 static int gCount[32];
 
 int Random(void);
 void Count(int random);
 
+/**
+ main
+
+ @param argc number of arguments
+ @param argv argument array
+ @return error code
+ */
 int main(
     int argc,
     const char * argv[])
 {
-    gRandom=(int)random();
+	size_t length=0;
+	int result=sysctlbyname("hw.optional.sse",NULL,&length,NULL,0);
+	assert(length==sizeof(int));
+	int sse=0;
+	result=sysctlbyname("hw.optional.sse",&sse,&length,NULL,0);
+	printf("sse=%d\n",sse);
+	
+	result=sysctlbyname("hw.optional.avx1_0",NULL,&length,NULL,0);
+	assert(length==sizeof(int));
+	int avx1=0;
+	result=sysctlbyname("hw.optional.avx1_0",&avx1,&length,NULL,0);
+	printf("avx1=%d\n",avx1);
+
+	result=sysctlbyname("hw.optional.avx2_0",NULL,&length,NULL,0);
+	assert(length==sizeof(int));
+	int avx2=0;
+	result=sysctlbyname("hw.optional.avx2_0",&avx2,&length,NULL,0);
+	printf("avx2=%d\n",avx2);
+
+	result=sysctlbyname("hw.optional.avx512f",NULL,&length,NULL,0);
+	assert(length==sizeof(int));
+	int avx512=0;
+	result=sysctlbyname("hw.optional.avx512f",&avx512,&length,NULL,0);
+	printf("avx512f=%d\n",avx512);
+
+	gRandom=(int)random();
     
     int i;
     
@@ -39,7 +94,11 @@ int main(
     return 0;
 }
 
-// note: bit 0 toggles - not random
+/**
+ Generate random number
+
+ @return int
+ */
 int Random()
 {
     gRandom=gRandom*1664525+1013904223;
@@ -47,6 +106,11 @@ int Random()
     return gRandom;
 }
 
+/**
+ Count the number of bits set
+
+ @param random int
+ */
 void Count(
     int random)
 {
